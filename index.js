92ec0de8e7ee2b3b0e688ff0a8956676d98b9b92
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-09-30 14:08:51 -0400

    Modified index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
+    readlineInterface.question(questionText, resolve);
   });
 }
 
-start();
+//start();
 
+//async function to start the game
 async function start() {
+  //console logs greeting to start the game
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+  //awaits human to enter a secret number
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  //console logs the secret number that you (human) pick
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+  //computer guesses secret number
+  await ask(`Is your secret number ${secretNumber}?`);
+  //console logs message re: correctly guessed number
+  console.log(`Your number was ${secretNumber}!`);
   process.exit();
 }
+
+//calls the function to start the game
+start();

876f8e515331b681d69be23a3eb9bbf013c054f5
JSaturn9 <69169169+JSaturn9@users.noreply.github.com>
2021-11-28 20:30:50 -0500

    Added functionality for reverse guess the number

--- index.js
+++ index.js
@@ -18,16 +18,15 @@ function updateGuess(min, max) {
   return Math.floor((max - min) / 2 + min);
 }
 
-let guess = randomNumber(1, 100);
+//to start the game min is equal to 1
 let min = 1;
-let max = 100;
 
 start(); //calls function to start the game
 
 //async function to start the game
 async function start() {
   //computer asks for a max range higher than 1
-  max = await ask(
+  let max = await ask(
     "Please enter a number for the maximum that is greater than 1. "
   );
   //console logs greeting to start the game
@@ -41,7 +40,9 @@ async function start() {
   //console logs the secret number that user inputs
   console.log("You entered: " + secretNumber);
   secretNumber = parseInt(secretNumber);
-  //console.log(typeof secretNumber);
+
+  //guess variable calls random number generator
+  let guess = randomNumber(min, max);
   //computer guesses number between min-max range
   let response = await ask(`Is your secret number ${guess}?`);
   //if the response is not "yes" and is not "y" the program starts looping asking if number is higher or lower;
@@ -49,32 +50,43 @@ async function start() {
   while (response.toLowerCase() !== "yes" && response.toLowerCase() !== "y") {
     //program asks if number is higher or lower
     let highOrLow = await ask("Is your number higher(h) or lower(l)?");
+    //if higher or lower response contradicts earlier response the game exits
+    if (guess === max && highOrLow === "h") {
+      console.log(
+        `You cheated! You said it was lower than ${
+          max + 1
+        }, so it can't also be higher than ${guess}.`
+      );
+      process.exit();
+    } else if (guess === min && highOrLow === "l") {
+      console.log(
+        `You cheated! You said it was higher than ${
+          min - 1
+        }, so it can't also be lower than ${guess}.`
+      );
+      process.exit();
+    }
 
     //if higher, a new minimum number is assigned to range
-    if (highOrLow.toLowerCase() === "higher" || highOrLow.toLowerCase() === "h") {
+    if (
+      highOrLow.toLowerCase() === "higher" ||
+      highOrLow.toLowerCase() === "h"
+    ) {
+      //sets highOrLow variable to string "h" so that any higher response is put to "h"
+      highOrLow = "h";
       //the min is set to current guess
       min = guess + 1;
     } else {
+      //sets highOrLow variable to string "l" so that any lower response is put to "l"
+      highOrLow = "l";
       //the max is set to current guess
       max = guess - 1;
     }
     //guess is set to updateGuess function, returning the middle value between the min and max values
-    // console.log(min);
-    // console.log(max);
-    // console.log(typeof guess);
     guess = updateGuess(min, max);
     //computer responds with updated guess
     response = await ask(`Is your secret number ${guess}?`);
-    //if higher or lower response contradicts earlier response the game exits
-    if (guess > max || guess < min) {
-      console.log("You cheated!");
-      process.exit;
-    }
   }
   //once correct number is guessed program exits while loop
   console.log(`HUZZAH YOU GUESSED IT! #${guess} PARTY TIME!`);
 }
-
-
-
-/*ROLE REVERSAL*/
--- reverse_game.js
+++ reverse_game.js
@@ -1,6 +1,48 @@
-//computer asks me (user) to guess a number
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
 
-//computer tells me if I guessed too low or too high
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, resolve);
+  });
+}
 
-//computer prompts me to guess again
+//random number generator function
+function randomNumber(min, max) {
+  let range = max - min + 1;
+  return Math.floor(Math.random() * range) + min;
+}
 
+//calls the function to start the game
+start();
+
+//async function to start the game
+async function start() {
+  let number = randomNumber(1, 10);
+  console.log(number);
+  //Computer asks me (user) to guess a number
+  let guess = await ask("Please guess a number between 1 - 10.\n");
+  //Turns the string guess variable into a number
+  guess = parseInt(guess);
+
+  while (guess !== number) {
+    //If the guess is greater than the number
+    if (guess > number) {
+      //Computer states the guess was too high
+      console.log(`I'm sorry, the number is lower than ${guess}.`);
+      //Computer prompts to guess again
+      guess = await ask("Please guess again...\n");
+      guess = parseInt(guess);
+      //If the guess is less than the number
+    } else if (guess < number) {
+      //Computer states the guess was too low
+      console.log(`I'm sorry, the number is higher than ${guess}.`);
+      //Computer prompts to guess again
+      guess = await ask("Please guess again...\n");
+      guess = parseInt(guess);
+    }
+  }
+  //If number is guessed correctly the computer congratulates the user
+  console.log(`Huzzah! You guessed correctly the number was ${number}!`);
+  process.exit();
+}
